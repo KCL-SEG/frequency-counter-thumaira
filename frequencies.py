fe31b3b2732e96ca0b833d64b122b7980ebb769d
Tahseen Humaira <humairatahseen4@gmail.com>
2022-12-02 10:49:12 +0000

    Update frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,9 @@
 
 def frequencies(items):
     frequencies = {}
+     
     # Your code goes here
+    for item in items:
+        frequencies[str(item)]= frequencies.get(str(item), 0)+1
+        #Will increment everytime an item is the same 
     return frequencies
